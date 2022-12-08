8bcde8aefe310e7f804d2b4846d151b4f5def35f
salmanchik1 <salmanov.ramis@gmail.com>
2022-12-08 18:29:35 +0300

    created and connected postgres database to the django

--- medieval_town/medieval_town/settings.py
+++ medieval_town/medieval_town/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'medieval_town.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medieval_town',
+        'USER': 'postgres',
+        'PASSWORD': 'Gareeva98-6',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
